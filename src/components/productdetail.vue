db2baab6d28adc88133a04f885ce89d8eb29d51f
bjy <youngblood0225@gmail.com>
2019-11-26 12:14:21 +0900

    product detail 페이지 수정중

--- src/components/productdetail.vue
+++ src/components/productdetail.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container">
-        <h1 style="text-align: center; font-size: 3em; margin-bottom:">제품 정보</h1>
+        <h1 style="text-align: center; font-size: 3em; margin-bottom:">제품 정보ㅋㅋ</h1>
         <hr style="margin-bottom: 40px;">
+
             <div class="row productRow">
                 <div class="col-xs-12 col-sm-5 col-md-4 col-lg-3">
                     <img
@@ -10,6 +11,7 @@
                         alt="food.name"
                         width="250"
                         style="margin: 0 auto;"></div>
+
                     <div class="col-xs-12 col-sm-7 col-md-8 col-lg-9">
                         <table class="table table-hover">
                             <tr>
@@ -27,6 +29,7 @@
                                 </td>
                             </tr>
                         </table>
+
                         <div class="input-group" style="float:right">
                             <span class="input-group-addon" id="basic-addon1">검색 빈도수 : </span>
                             {{food.searchCount}}
@@ -40,55 +43,76 @@
                                     <i class="glyphicon glyphicon-tag"></i>
                                     담기
                                 </div>
+                                <div
+                                    id="drawchart"
+                                    class="btn btn-flat btn-info"
+                                    role="button"
+                                    @click="drawchart()">
+                                    <i class="glyphicon glyphicon-tag"></i>
+                                    클릭
+                                </div>
                             </span>
                         </div>
                         <div class="btn-group" role="group"></div>
                     </div>
                 </div>
+
                 <h1 style="text-align: center; font-size: 3em;">영양 정보</h1>
+
                 <hr style="margin-bottom: 40px;">
                     <div class="row">
-                        <div class="col-sm-8">
-                            <canvas id="myChart2" class=""></canvas>
+                        <div class="col-sm-8"  v-if="ReceivedData">
+                            <h1> HELLLO </h1>
+                            <canvas id="myChart2"></canvas>
                         </div>
+
                         <div class="col-sm-4">
                             <table class="table">
                                 <tr>
                                     <td>일일 제공량</td>
                                     <td id="SERVING_WT">{{food.supportpereat }}</td>
                                 </tr>
+
                                 <tr>
                                     <td>칼로리</td>
                                     <td id="NUTR_CONT1">{{food.calory }}</td>
                                 </tr>
+
                                 <tr>
                                     <td>탄수화물</td>
                                     <td id="NUTR_CONT2">{{food.carbo }}</td>
                                 </tr>
+
                                 <tr>
                                     <td>단백질</td>
                                     <td id="NUTR_CONT3">{{food.protein }}</td>
                                 </tr>
+
                                 <tr>
                                     <td>지방</td>
                                     <td id="NUTR_CONT4">{{food.fat }}</td>
                                 </tr>
+
                                 <tr>
                                     <td>당류</td>
                                     <td id="NUTR_CONT5">{{food.sugar }}</td>
                                 </tr>
+
                                 <tr>
                                     <td>나트륨</td>
                                     <td id="NUTR_CONT6">{{food.natrium }}</td>
                                 </tr>
+
                                 <tr>
                                     <td>콜레스테롤</td>
                                     <td id="NUTR_CONT7">{{food.chole }}</td>
                                 </tr>
+
                                 <tr>
                                     <td>포화 지방산</td>
                                     <td id="NUTR_CONT8">{{food.fattyacid }}</td>
                                 </tr>
+
                                 <tr>
                                     <td>트렌스지방</td>
                                     <td id="NUTR_CONT9">{{food.transfat }}</td>
@@ -98,6 +122,7 @@
                     </div>
                 </div>
             </template>
+
             <script>
                 import http from "../http-common";
                 // import App from "../App.vue";
@@ -114,8 +139,15 @@
                             loading: true,
                             errored: false,
                             id: 'orora@com',
-                            icount: 0
+                            icount: 0,
+                            ReceivedData: 'false'
+
                         };
+                    },
+                    computed: {
+                        checkDataVar: function(){
+                            return this.ReceivedData;
+                        }
                     },
                     methods: {
                         dynamicColors() {
@@ -125,6 +157,7 @@
                             return "rgba(" + r + "," + g + "," + b + ",0.8)";
                         },
                         drawchart() {
+                            this.ReceivedData = true;
                             const myChart2 = new Chart("myChart2", {
                                 type: 'doughnut',
                                 data: {
@@ -181,13 +214,16 @@
                                 }
                             });
                             myChart2;
+
                         },
                         infofood() {
                             http
                                 .get("/infofood/" + this.code)
                                 .then(response => {
                                     this.food = response.data.food;
-                                    this.drawchart();
+                                    this.ReceivedData = true;
+                                   
+
                                 })
                                 .catch(() => {
                                     alert("fail" + this.code);
@@ -197,22 +233,29 @@
                         },
                         insertintake() {
                             this.user_id = localStorage.getItem("id");
+
                             http
                                 .get("/insertintake/" + this.user_id + "/" + this.food.code)
                                 .then(response => {
                                     this.res = response.data.resvalue;
+
                                     alert(response.data.resvalue);
+
                                 })
                                 .catch(() => {
                                     alert("fail" + this.user_id + " " + this.food.code)
                                     this.errored = true;
                                 })
                                 . finally(() => (this.loading = false));
+
                         }
                     },
+
                     filters: {},
                     mounted() {
                         this.infofood();
+                         this.drawchart();
+                        this.ReceivedData = false;
                     }
                 };
             </script>
