21f24309f1cd6c4b3bbf3bd04ab3796ef589f2c8
OJY <orora5411@gmail.com>
2019-11-28 10:32:06 +0900

    공지사항 살짝밑으로

--- src/components/viewNotice.vue
+++ src/components/viewNotice.vue
@@ -1,6 +1,6 @@
 <template>
  <div class="container">
-<h1 style="text-align: center; font-size: 3em; margin-bottom:">공지 사항 </h1>
+<h1 style="text-align: center; font-size: 3em; margin-top:40px;">공지 사항 </h1>
 		<hr style="margin-bottom: 40px;">
 		
 		<div class="panel panel-info" style="background-color: rgba( 255, 255, 255,0.8 ); height: 800px; ">
